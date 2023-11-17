9ae1db5be328218e4b486549e15091b2f396372c
MichLim <71734957+MichLim@users.noreply.github.com>
2023-11-17 16:41:12 -0500

    Create common.js
    Includes Navbar& footer so we don't duplicate code
    
    Signed-off-by: MichLim <71734957+MichLim@users.noreply.github.com>

---
+++ js/common.js
@@ -0,0 +1,18 @@
+function includeNavbar() {
+    document.write(`
+        <header>
+            <h1>Michelle Lim</h1>
+        </header>
+    
+        <nav class="nav nav-pills justify-content-center">
+            <a class="nav-item nav-link" href="about.html">About Me</a>
+            <a class="nav-item nav-link" href="projects.html">Projects</a>
+            <a class="nav-item nav-link" href="contact.html">Contact</a>
+            <a class="nav-item nav-link" href="fun.html">Fun Stuff</a>
+        </nav>
+    
+        <footer>
+            <p>&copy; 2023 Michelle Lim</p>
+        </footer>
+    `);
+}
