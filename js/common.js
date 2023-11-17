8309b11955517d82d11b0cc5c98c64514b5c77e2
MichLim <lim.miche@northeastern.edu>
2023-11-17 18:29:21 -0500

    deleted contact, added contact to about

--- js/common.js
+++ js/common.js
@@ -3,13 +3,12 @@
 function includeNavbar() {
     document.write(`
         <header>
-            <h1>Michelle Lim</h1>
+            <h1><a href="index.html">Michelle Lim</a></h1>
         </header>
     
         <nav class="nav nav-pills justify-content-center">
             <a class="nav-item nav-link ${isActivePage('about.html')}" href="about.html">About Me</a>
             <a class="nav-item nav-link ${isActivePage('projects.html')}" href="projects.html">Projects</a>
-            <a class="nav-item nav-link ${isActivePage('contact.html')}" href="contact.html">Contact</a>
             <a class="nav-item nav-link ${isActivePage('fun.html')}" href="fun.html">Fun Stuff</a>
         </nav>
     `);
