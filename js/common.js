ba3f231daf1fd2e91f0510606e53a4b8e438b0e4
MichLim <lim.miche@northeastern.edu>
2023-11-17 17:49:46 -0500

    added some nav bar colors

--- js/common.js
+++ js/common.js
@@ -7,13 +7,26 @@ function includeNavbar() {
         </header>
     
         <nav class="nav nav-pills justify-content-center">
-            <a class="nav-item nav-link" href="about.html">About Me</a>
-            <a class="nav-item nav-link" href="projects.html">Projects</a>
-            <a class="nav-item nav-link" href="contact.html">Contact</a>
-            <a class="nav-item nav-link" href="fun.html">Fun Stuff</a>
+            <a class="nav-item nav-link ${isActivePage('about.html')}" href="about.html">About Me</a>
+            <a class="nav-item nav-link ${isActivePage('projects.html')}" href="projects.html">Projects</a>
+            <a class="nav-item nav-link ${isActivePage('contact.html')}" href="contact.html">Contact</a>
+            <a class="nav-item nav-link ${isActivePage('fun.html')}" href="fun.html">Fun Stuff</a>
         </nav>
     `);
 }
+
+function isActivePage(pageName) {
+    // Get the current page URL
+    let currentPage = window.location.href;
+
+    // Check if the current page URL contains the page name
+    if (currentPage.includes(pageName)) {
+        return 'active';
+    } else {
+        return '';
+    }
+}
+
 
 function includeFooter() {
     document.write(`
